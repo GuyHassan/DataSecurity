1ef83735a8b0fc5c643eed110c8bdaf8cc5f819d
yakiryacov <yakiryacov@gmail.com>
2019-04-07 14:57:46 +0300

    yakir changes

--- createUrlPage.py
+++ createUrlPage.py
@@ -1,35 +1,39 @@
 import bs4 as bs
 import urllib.request
+from projectSecurity import setLinkSite
 
 # to know if there was an error
 class createUrlPage:
     def __init__(self):
-      self.err = 0
-      self.tempList = []
-    def geturl(self):
+        self.err = 0
+
+    def geturl(self, url):
         ### i will use a while loop until templist is not empty and every time
         ### i will pop the first link and go into, i will put the data again in sauce and soup
         ### then i will try to manage it with a global variable to check that i dont go deep to much
         try:
-            sauce = urllib.request.urlopen('https://192-168-1-1ip.mobi/default-router-passwords-list/').read()
+            sauce = urllib.request.urlopen(url).read()
             soup = bs.BeautifulSoup(sauce, 'lxml')
         except:
             print("cant connect to this url")
             err = 1
             return -1
+        urls = []
         if self.err == 0:
             for url in soup.find_all('a'):
                 if (url.get('href') is not None):
                     if 'https' in url.get('href'):  # filter to only links
-                        self.tempList.append(url.get('href'))
-
-    def printURL(self):
-        print(self.tempList[0])
-        print(self.tempList[1])
-        print(self.tempList[2])
-        print(self.tempList[3])
+                        urls.append(url.get('href'))
+        return urls
 
 
+    def GoDeep(self, url, depth):
+        if(dept == 0):
+            return
+        urls = self.geturl(url)
+        for currUrl in urls:
+            GoDeep(currUrl, depth - 1)
+            setLinkSite(currUrl)
 
 
 UrlPage = createUrlPage()
