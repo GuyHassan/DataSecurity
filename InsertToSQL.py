375cb1e096cbfdb1cabe812be9a6775952627516
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2019-04-14 18:12:42 +0300

    finish

--- InsertToSQL.py
+++ InsertToSQL.py
@@ -1,7 +1,7 @@
 import bs4 as bs
 import urllib.request
 from SQLiteClass import MySQLiteDB
-from testingNew import SearchSecurityCourse
+from projectSecurity import SearchSecurityCourse
 
 listOfDataFromSite = []
 objectSecurity = SearchSecurityCourse()
@@ -10,7 +10,7 @@ newDB = MySQLiteDB()
 link = 'https://www.a1securitycameras.com/technical-support/default-username-passwords-ip-addresses-for-surveillance-cameras/'
 link2 = 'http://www.phenoelit.org/dpl/dpl.html'
 listOfDataFromSite.extend(objectSecurity.setLinkSite(link))
-listOfDataFromSite.extend(objectSecurity.setLinkSite(link2))
+# listOfDataFromSite.extend(objectSecurity.setLinkSite(link2))
 
 for line in listOfDataFromSite:
     if ('dib' not in line['Model'] and 'dib' not in line['Username'] and 'dib' not in line['Password']):
--- createUrlPage.py
+++ createUrlPage.py
@@ -1,37 +1,45 @@
 import bs4 as bs
 import urllib.request
+import projectSecurity
 
 # to know if there was an error
 class createUrlPage:
     def __init__(self):
-      self.err = 0
-      self.tempList = []
-    def geturl(self):
+        self.err = 0
+
+    def geturl(self, url):
         ### i will use a while loop until templist is not empty and every time
         ### i will pop the first link and go into, i will put the data again in sauce and soup
         ### then i will try to manage it with a global variable to check that i dont go deep to much
         try:
-            sauce = urllib.request.urlopen('https://192-168-1-1ip.mobi/default-router-passwords-list/').read()
+            sauce = urllib.request.urlopen(url).read()
             soup = bs.BeautifulSoup(sauce, 'lxml')
         except:
             print("cant connect to this url")
             err = 1
             return -1
+        urls = []
         if self.err == 0:
             for url in soup.find_all('a'):
                 if (url.get('href') is not None):
                     if 'https' in url.get('href'):  # filter to only links
-                        self.tempList.append(url.get('href'))
-
-    def printURL(self):
-        print(self.tempList[0])
-        print(self.tempList[1])
-        print(self.tempList[2])
-        print(self.tempList[3])
+                        if url.get('href') not in urls:
+                            urls.append(url.get('href'))
+        return urls
 
 
+    def GoDeep(self, url, depth):
+        if depth == 0:
+            return
+        ssc = projectSecurity.SearchSecurityCourse()
+        print('Current: ' + url)
+        default = ssc.setLinkSite(url)
+        urls = self.geturl(url)
+        urls = urls[:4]
+        print(urls)
+        for currUrl in urls:
+            default.append(self.GoDeep(str(currUrl), depth - 1))
+        return default
 
-
-UrlPage = createUrlPage()
-if UrlPage.geturl() != -1:
-    UrlPage.printURL()
+cup = createUrlPage()
+print(cup.GoDeep("https://www.a1securitycameras.com/technical-support/default-username-passwords-ip-addresses-for-surveillance-cameras/", 2))
